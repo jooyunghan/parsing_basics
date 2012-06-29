1b6f34e578de9f2b1a8600c86ef2e4d92895dbc4
Jooyung Han <jooyung.han@gmail.com>
2012-06-29 16:01:09 +0900

    desktop calc version 1: loop and multiple operand
    2+2*3 => 12 (wrong)

--- samples/calculator0/dc.cpp
+++ samples/calculator0/dc.cpp
@@ -5,16 +5,21 @@ using namespace std;
 int main() {
 	int left, right;
 	char op;
-	cin >> left >> op >> right;
-
-	int result;
-	if (op == '+')
-		result = left + right;
-	else if (op == '-')
-		result = left - right;
-	else 
-		result = 0;
-
-	cout << result << endl;
+	cout << "example: 1+2+3 (including ';')" << endl << "> ";
+	cin >> left;
+	while (cin >> op) {
+		if (op != ';') { cin >> right; }
+		switch (op) {
+		case '+': left += right; break;
+		case '-': left -= right; break;
+		case '*': left *= right; break;
+		case '/': left /= right; break;
+		default : 
+			cout << "= " << left << endl; 
+			cout << "example: 1+2+3 (including ';')" << endl << "> ";
+			cin >> left;
+			break;
+		}
+	}
 	return 0;
 }
